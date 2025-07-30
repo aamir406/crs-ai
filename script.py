ce00d9f451a021808647231253cd2cb11f95b4b0
Aamir <m.aamir407.ali@gmail.com>
2025-07-30 15:38:05 +0400

    identifies boxes with red borders. dummy test image

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/test_page.png')
+img = cv2.imread('images/test_image.png')
+
+test = img
 
 cv2.imshow("pic", img)
 
@@ -19,10 +21,29 @@ mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
 mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
 
 # apply final masks
-mask = cv2.bitwise_or(mask1, mask2)
-target = cv2.bitwise_and(img, img, mask=mask)
+mask_red = cv2.bitwise_or(mask1, mask2)
+# mask_red = cv2.bitwise_and(img, img, mask=mask)
 
-cv2.imwrite("target.png", target)
+contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+boxes = []
+for c in contours:
+    approx = cv2.approxPolyDP(c, 0.02*cv2.arcLength(c, True), True)
+    x, y, w, h = cv2.boundingRect(approx)
+
+    if len(approx) == 4 and w*h > 5000:
+        boxes.append((x, y, w, h))
+        cv2.rectangle(test, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Detected Boxes", test)
+cv2.waitKey(3000)
+cv2.destroyAllWindows()
+
+
+
+
+
+# cv2.imwrite("target.png", target)
 
 
 print(img)
